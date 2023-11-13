ae61e587d32b6e389acbf09211c65bdc1d084fc1
Arnau8844 <arnau.mateuf@educem.net>
2023-11-13 09:38:28 +0100

    Hola perra

---
+++ app/src/main/java/com/example/practica_grancentre/activity_restaurant.java
@@ -0,0 +1,14 @@
+package com.example.practica_grancentre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_restaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
